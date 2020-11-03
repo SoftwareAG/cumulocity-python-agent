eedc8dbc88694fc1ad8142491ed247a3a62a407c
Bayram <y509177@softwareag.com>
2020-11-03 16:05:35 +0100

    Added info text for bluk registration

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 docker-compose up -d --scale agent=$1
+echo "You can copy everything below this line and save as CSV file to use bulk registration!"
 echo "ID"
 docker stats --no-stream --format "{{.Container}}"
