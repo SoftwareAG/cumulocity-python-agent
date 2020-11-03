7c590043dffadc29423ca04bb5a0fb37942c1395
Bayram <y509177@softwareag.com>
2020-11-03 16:26:54 +0100

    Added --build into compose to have effect of changes within the code

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-docker-compose up -d --scale agent=$1
+docker-compose up --build -d --scale agent=$1
 echo "You can copy everything below this line and save as CSV file to use bulk registration!"
 echo "ID;PATH"
 docker stats --no-stream --format "{{.Container}};"
